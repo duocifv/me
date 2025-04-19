5413bf627e7cdb8b0778fe5545f085bac7ea88be
duocifv <41617446+duocifv@users.noreply.github.com>
2025-04-19 16:01:57 +0700

    update

--- generated/prisma/edge.js
+++ generated/prisma/edge.js
@@ -134,7 +134,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
+    "rootEnvPath": "../../.env",
     "schemaEnvPath": "../../.env"
   },
   "relativePath": "../../prisma",
