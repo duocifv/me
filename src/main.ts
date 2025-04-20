f7487649f9a2ba2c09bb47e57f7a610903f70bba
duocifv <41617446+duocifv@users.noreply.github.com>
2025-04-20 08:52:37 +0700

    deploy fastify

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 5000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  // (Optional) If you need CORS, pipes, etc. you can enable them here:
+  // app.enableCors();
+  // app.useGlobalPipes(new ValidationPipe());
+
+  const port = process.env.PORT ? +process.env.PORT : 5000;
+  // Listen on all interfaces (often needed in Docker / Cloud)
+  await app.listen(port, '0.0.0.0');
+
+  console.log(`🚀 Server ready at http://localhost:${port}`);
 }
+
 bootstrap();
