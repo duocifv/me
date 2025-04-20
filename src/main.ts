8c909c9cf2c804f6c4086a822a11c2bbf61aeedd
duocifv <41617446+duocifv@users.noreply.github.com>
2025-04-20 09:50:21 +0700

    expresss test

--- src/main.ts
+++ src/main.ts
@@ -8,20 +8,8 @@ import {
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter({
-      ignoreTrailingSlash: true,
-    }),
+    new FastifyAdapter(),
   );
-
-  // (Optional) If you need CORS, pipes, etc. you can enable them here:
-  // app.enableCors();
-  // app.useGlobalPipes(new ValidationPipe());
-
-  const port = process.env.PORT ? +process.env.PORT : 5000;
-  // Listen on all interfaces (often needed in Docker / Cloud)
-  await app.listen(port, '0.0.0.0');
-
-  console.log(`🚀 Server ready at http://localhost:${port}`);
+  await app.listen(5000);
 }
-
 bootstrap();
