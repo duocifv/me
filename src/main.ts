0502e018ca76c044e1970432a463c577d5a41522
duocdgw <duoc-nv@dgw.asia>
2025-04-22 16:57:59 +0700

    up

--- src/main.ts
+++ src/main.ts
@@ -9,8 +9,7 @@ async function bootstrap() {
 
   try {
     await app.listen({ port, host: '0.0.0.0' });
-    console.log(`🚀 Server is running at http://localhost:${port}`);
-    // app.log.info(`🚀 Server is running at ${address}`);
+    app.log.info(`🚀 Server is running at http://localhost:${port}`);
   } catch (err) {
     app.log.error(err);
     process.exit(1);
