e00af7240b403ef75fd5c0246e64e831601156ff
duocdgw <duoc-nv@dgw.asia>
2025-04-22 17:09:33 +0700

    okkkkkkkkkkk

--- src/schema/config.ts
+++ src/schema/config.ts
@@ -3,7 +3,6 @@ import mysql from "mysql2/promise";
 import dotenv from 'dotenv';
 dotenv.config();
 
-const url = new URL(process.env.DATABASE_URL!);
 const pool = mysql.createPool(process.env.DATABASE_URL!); 
 export const db = drizzle(pool);
 
@@ -11,7 +10,4 @@ pool.getConnection().then(() => {
   console.log("✅ Connected to DB");
 }).catch((err) => {
   console.error("❌ DB Connection Error:", err);
-  console.log('🔍 DB config', {
-    url
-  });
 });
