7c64a1106dde52fbf1ace05e57e183dfdcac484b
duocdgw <duoc-nv@dgw.asia>
2025-07-17 11:14:57 +0700

    deploy4

---
+++ ai_py/README.md
@@ -0,0 +1,12 @@
+---
+title: Duoc2
+emoji: 🦀
+colorFrom: blue
+colorTo: red
+sdk: docker
+pinned: false
+license: mit
+short_description: duoc2
+---
+
+Check out the configuration reference at https://huggingface.co/docs/hub/spaces-config-reference
---
+++ ai_py/app.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI, Body
+from pydantic import BaseModel
+
+app = FastAPI()
+
+# Kiểu dữ liệu đầu vào
+class Item(BaseModel):
+    id: int
+    name: str
+    description: str = None
+
+# GET /
+@app.get("/")
+def root():
+    return {"Hello": "World!"}
+
+# POST /items
+@app.post("/items")
+def create_item(item: Item):
+    return {"message": "Item created", "item": item}
+
+# PUT /items/{item_id}
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {
+        "message": f"Item {item_id} updated",
+        "item": item
+    }
