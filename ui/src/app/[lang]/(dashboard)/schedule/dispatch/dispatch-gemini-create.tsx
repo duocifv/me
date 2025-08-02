ee61298847e4537510c3352acd4388f9e6d3c903
duocifv <41617446+duocifv@users.noreply.github.com>
2025-08-02 14:06:23 +0700

    Update dispatch-gemini-create.tsx
    d

--- ui/src/app/[lang]/(dashboard)/schedule/dispatch/dispatch-gemini-create.tsx
+++ ui/src/app/[lang]/(dashboard)/schedule/dispatch/dispatch-gemini-create.tsx
@@ -2,9 +2,9 @@ import { Button } from "@/components/ui/button";
 import { Loader2 } from "lucide-react";
 import { useCreateGeminiMutation } from "@/module/device/device.hook.";
 
+
 export default function CreateGeminiSubmit() {
   const { mutate, isPending } = useCreateGeminiMutation()
- 
   return (
     <Button
       onClick={mutate}
