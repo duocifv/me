16fd3ae595a5da7cd0ac691753049ce9c55454bf
nvduocfpt <nvduocfpt@gmail.com>
2025-08-27 13:17:38 +0700

    dsadas

--- ui/src/app/[lang]/(auth)/chat/page.tsx
+++ ui/src/app/[lang]/(auth)/chat/page.tsx
@@ -1,12 +1,10 @@
 "use client";
-import ChatWidget from "./src_app/ChatWidget";
 import HotelDashboard from "./src_app/HotelDashboard";
 import FetchRooms from "./src_app/service/fetch";
 
 export default function ChatPage() {
   return (
     <div>
-      <ChatWidget />
       <FetchRooms />
       <HotelDashboard />
     </div>
