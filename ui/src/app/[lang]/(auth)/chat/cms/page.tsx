4696b73258ea43a48223149c54fefdb0bf6df3d7
nvduocfpt <nvduocfpt@gmail.com>
2025-08-27 17:33:22 +0700

    dsad23421sad

--- ui/src/app/[lang]/(auth)/chat/cms/page.tsx
+++ ui/src/app/[lang]/(auth)/chat/cms/page.tsx
@@ -9,7 +9,16 @@ export default function ChatPage() {
       <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
         <div className="flex justify-between items-center px-4 lg:px-6">
           <div>
-            <h2 className="text-2xl font-semibold">Khách đặt phòng</h2>
+            <h2 className="text-2xl font-semibold">
+              Khách đặt phòng
+              <a
+                href="https://docs.google.com/spreadsheets/d/1omb7VK2LlTHuFptEOaIAq7BsIX-ifiVEIghqQEX1piY/edit?gid=1009060172#gid=1009060172"
+                target="_blank"
+                rel="noreferrer"
+              >
+                [Google Excel]
+              </a>
+            </h2>
           </div>
         </div>
         <Bookings />
