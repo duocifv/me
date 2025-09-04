47ce28e8fb5585bef72c6d9f3aaa0212ecc95e3d
nvduocfpt <nvduocfpt@gmail.com>
2025-09-04 16:38:02 +0700

    deploy567

--- astro/retrograde-remnant/src/pages/rss.xml.js
+++ astro/retrograde-remnant/src/pages/rss.xml.js
@@ -1,16 +1,23 @@
-import { getCollection } from 'astro:content';
-import rss from '@astrojs/rss';
-import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
+import rss from "@astrojs/rss";
+import { SITE_DESCRIPTION, SITE_TITLE } from "../consts";
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.id}/`,
-		})),
-	});
+  // Lấy dữ liệu từ API
+  const posts = await fetch(
+    "https://vegetable-container.onrender.com/v1/agent/blogs"
+  )
+    .then((res) => res.json())
+    .catch(() => []);
+
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: "https://novu.pages.dev",
+    items: posts.map((post) => ({
+      title: post.title,
+      pubDate: post.createdAt,
+      description: post.intro,
+      link: `/blog/${post.id}/`,
+    })),
+  });
 }
